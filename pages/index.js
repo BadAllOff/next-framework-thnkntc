e0f89b73351be7a597a1bbed3713515190905c97
Shamil <shamil.badalloffsh@gmail.com>
2020-08-20 19:14:42 +0200

    Modify: index.js

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
-export default function Home() {
+import BookList from "../components/Book/List/BookList";
+import ScrollButton from "../components/helpers/ScrollButton";
+import Layout from "../components/Layout";
+import withBooks from "../HOC/withBooks";
+import withLoader from "../HOC/withLoader";
+import user from "../data/user.json";
+import AuthContext from "../components/meta/AuthContext";
+
+function Home(props) {
   return (
-    <div className={styles.container}>
+    <AuthContext.Provider value={user}>
       <Head>
         <title>Next Blog Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <Layout>
+        {props.books && (
+          <BookList books={props.books} isLoading={!props.books} />
+        )}
+        <ScrollButton
+          scrollStepInPx="50"
+          delayInMs="16.66"
+          ShowAtPosition={global.innerHeight / 3}
+          TransitionClassName="visible"
+        />
+      </Layout>
+      <div id="modal-root"></div>
+    </AuthContext.Provider>
+  );
 }
+
+export async function getServerSideProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
+
+export default withBooks(withLoader(Home));
