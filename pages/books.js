20d761ed1d6dcbb85730e2811393030bc924ebaa
Shamil <shamil.badalloffsh@gmail.com>
2020-08-20 18:18:32 +0200

    Fix: user context

--- pages/books.js
+++ pages/books.js
@@ -3,23 +3,27 @@ import ScrollButton from "../components/helpers/ScrollButton";
 import Layout from "../components/Layout";
 import withBooks from "../HOC/withBooks";
 import withLoader from "../HOC/withLoader";
+import user from "../data/user.json";
+import AuthContext from "../components/meta/AuthContext";
 
 function Books(props) {
   return (
     <div>
-      <Layout>
-        {/* <SimilarBooksList similarBooks={this.props.similarBooks} /> */}
-        {props.books && (
-          <BookList books={props.books} isLoading={!props.books} />
-        )}
-        <ScrollButton
-          scrollStepInPx="50"
-          delayInMs="16.66"
-          ShowAtPosition={global.innerHeight / 3}
-          TransitionClassName="visible"
-        />
-      </Layout>
-      <div id="modal-root"></div>
+      <AuthContext.Provider value={user}>
+        <Layout>
+          {/* <SimilarBooksList similarBooks={this.props.similarBooks} /> */}
+          {props.books && (
+            <BookList books={props.books} isLoading={!props.books} />
+          )}
+          <ScrollButton
+            scrollStepInPx="50"
+            delayInMs="16.66"
+            ShowAtPosition={global.innerHeight / 3}
+            TransitionClassName="visible"
+          />
+        </Layout>
+        <div id="modal-root"></div>
+      </AuthContext.Provider>
     </div>
   );
 }
