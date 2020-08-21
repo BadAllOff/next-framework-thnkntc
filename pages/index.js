ffcce8f7916a89b26dd357fd4b708a3913e3a875
Shamil <shamil.badalloffsh@gmail.com>
2020-08-21 15:03:49 +0200

    Add: util\helper functions\mixins

--- pages/index.js
+++ pages/index.js
@@ -6,62 +6,10 @@ import Layout from "../components/Layout";
 import withLoader from "../HOC/withLoader";
 import user from "../data/user.json";
 import AuthContext from "../components/meta/AuthContext";
-import axios from "axios";
-import { zip, zipObject } from "lodash";
+import {fetchData} from "../utils/fetch-data"
 
-const API_TOKEN = "key9ncgesGi9whRNC";
 
-const httpClient = axios.create({
-  baseURL: "https://api.airtable.com/v0/applIXFkfNWeoU7uh",
-  timeout: 5000,
-  headers: {
-    Authorization: `Bearer ${API_TOKEN}`,
-  },
-});
-
-function _mapFromAirtable({ records }) {
-  return records.map((record) => ({
-    id: record.fields.id,
-    title: record.fields.title,
-    description: record.fields.description,
-    pageCount: record.fields.page_count,
-    language: record.fields.language,
-    progress: record.fields.progress,
-    coverImage: record.fields.cover_image[0].url,
-    authorList: _mapAuthorsForEachRecord(record),
-    minPrice: record.fields.min_price,
-    mainPrice: record.fields.main_price,
-    totalSum: record.fields.total_sum,
-    expectedSum: record.fields.expected_sum,
-    subscribersCount: record.fields.subscribers_count,
-  }));
-}
-
-function _mapAuthorsForEachRecord(record) {
-  return record.fields.authors
-    ? (() => {
-        let arr = [];
-
-        if (record.fields.authors.length > 0) {
-          for (let i = 0; i < record.fields.authors.length; i++) {
-            arr = zip(
-              record.fields["id (from authors)"],
-              record.fields["name (from authors)"],
-              record.fields["email (from authors)"],
-              record.fields["about (from authors)"],
-              record.fields["avatar (from authors)"].map((item) => item.url)
-            ).map((record) =>
-              zipObject(["id", "name", "email", "about", "avatar"], record)
-            );
-          }
-        }
-
-        return arr;
-      })()
-    : [];
-}
-
-function Home({books}) {
+function Home({ books }) {
   return (
     <AuthContext.Provider value={user}>
       <Head>
@@ -69,9 +17,7 @@ function Home({books}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        {books && (
-          <BookList books={books} isLoading={!books} />
-        )}
+        {books && <BookList books={books} isLoading={!books} />}
         <ScrollButton
           scrollStepInPx="50"
           delayInMs="16.66"
@@ -85,21 +31,11 @@ function Home({books}) {
 }
 
 export async function getServerSideProps(context) {
-  return httpClient
-    .get("/books", {
-      maxRecords: 3,
-      view: "Grid view",
-    })
-    .then((result) => result.data)
-    .then((data) => {
-      return _mapFromAirtable(data);
-    })
-    .then((books) => {
-      return {
-        props: { books }, // will be passed to the page component as props
-      };
-    });
+  return {
+    props: {
+      books: await fetchData(),
+    },
+  };
 }
-
 
 export default withLoader(Home);
